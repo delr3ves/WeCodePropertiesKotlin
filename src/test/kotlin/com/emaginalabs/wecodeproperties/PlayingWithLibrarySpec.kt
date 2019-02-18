b8d24f609d9bc0eb067db02f8d2dc78bc75edfb7
Sergio Arroyo <sergio.arroyo.cuevas@gmail.com>
2019-02-19 00:09:52 +0100

    Fix styles

--- src/test/kotlin/com/emaginalabs/wecodeproperties/PlayingWithLibrarySpec.kt
+++ src/test/kotlin/com/emaginalabs/wecodeproperties/PlayingWithLibrarySpec.kt
@@ -10,13 +10,13 @@ class PlayingWithLibrarySpec : ShouldSpec() {
         "Playing with the library" {
             should("add logs to know how it executes") {
                 forAll { a: Int, b: Int ->
-                    //write test the whatever property about the sum but add logs to know the value of the received numbers
+                    // write test the whatever property about the sum but add logs to know the value of the received numbers
                     true
                 }
             }
 
             should("allow us to modify the number of executions") {
-                //write a test in order to ensure the the number of executions
+                // write a test in order to ensure the the number of executions
                 // Here, you'll find how to change the number of success executions: https://stackoverflow.com/a/38706052
             }
 
@@ -24,7 +24,7 @@ class PlayingWithLibrarySpec : ShouldSpec() {
                 var failed = false
                 try {
                     forAll { a: Int, b: Int ->
-                        //write a test that sometimes fail and add traces in order to know how the library tries to give you a clue about the failing data
+                        // write a test that sometimes fail and add traces in order to know how the library tries to give you a clue about the failing data
                         fail("Just fail")
                     }
                 } catch (e: AssertionError) {
@@ -34,13 +34,13 @@ class PlayingWithLibrarySpec : ShouldSpec() {
             }
 
             should("fail when a generator is too restrictive") {
-                //write a custom generator that is too restrictive to find enough values to run the tests.
-                //Tip: You can capture the error the same way the previous test :)
+                // write a custom generator that is too restrictive to find enough values to run the tests.
+                // Tip: You can capture the error the same way the previous test :)
             }
 
             should("fail when a generator is limited but not restrictive") {
-                //write a custom generator that only generate a very limited range of values.
-                //Tip: You can use an already defined generator or just implement yours. I'll do mine :).
+                // write a custom generator that only generate a very limited range of values.
+                // Tip: You can use an already defined generator or just implement yours. I'll do mine :).
             }
         }
     }
