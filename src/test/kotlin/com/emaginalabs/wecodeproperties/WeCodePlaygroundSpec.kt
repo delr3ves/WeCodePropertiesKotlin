e9f2914f1e0fef67f809a750f899a582a7ab4499
Sergio Arroyo <sergio.arroyo.cuevas@gmail.com>
2019-02-18 23:53:23 +0100

    Implement properties of the sum

--- src/test/kotlin/com/emaginalabs/wecodeproperties/WeCodePlaygroundSpec.kt
+++ src/test/kotlin/com/emaginalabs/wecodeproperties/WeCodePlaygroundSpec.kt
@@ -1,7 +1,11 @@
 package com.emaginalabs.wecodeproperties
 
-import io.kotlintest.specs.ShouldSpec
+import io.kotlintest.properties.forAll
 import io.kotlintest.shouldBe
+import io.kotlintest.specs.ShouldSpec
+import io.kotlintest.tables.headers
+import io.kotlintest.tables.row
+import io.kotlintest.tables.table
 
 class WeCodePlaygroundSpec : ShouldSpec() {
 
@@ -10,16 +14,37 @@ class WeCodePlaygroundSpec : ShouldSpec() {
     init {
         "Sum properties" {
             should("keep the associative property") {
-                //TODO test the associative property
+                forAll { a: Int, b: Int, c: Int ->
+                    sum(sum(a, b), c) == sum(a, sum(b, c))
+                }
             }
+
             should("keep the commutative property") {
-                //TODO test the commutative property
+                forAll { a: Int, b: Int, c: Int ->
+                    sum(a, b) == sum(b, a)
+                }
             }
+
             should("have an identity value") {
-                //TODO test the identity property
+                forAll { a: Int ->
+                    sum(a, 0) == a
+                }
             }
+
+            val sumCases = table(
+                headers("a", "b", "result"),
+                row(1, 4, 5),
+                row(4, 5, 9),
+                row(0, 0, 0),
+                row(-1, -5, -6),
+                row(1, -5, -4),
+                row(-5, 1, -4)
+            )
+
             should("actually sum a couple numbers") {
-                //TODO test the sum
+                io.kotlintest.tables.forAll(sumCases) { a: Int, b: Int, expected: Int ->
+                    a + b shouldBe expected
+                }
             }
         }
     }
