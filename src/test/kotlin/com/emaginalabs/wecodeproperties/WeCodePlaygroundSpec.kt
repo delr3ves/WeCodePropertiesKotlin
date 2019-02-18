90f45a57878e4150927db085befc822bb667157c
Sergio Arroyo <sergio.arroyo.cuevas@gmail.com>
2019-02-18 23:43:13 +0100

    Define operations to do with the Sum

--- src/test/kotlin/com/emaginalabs/wecodeproperties/WeCodePlaygroundSpec.kt
+++ src/test/kotlin/com/emaginalabs/wecodeproperties/WeCodePlaygroundSpec.kt
@@ -5,10 +5,21 @@ import io.kotlintest.shouldBe
 
 class WeCodePlaygroundSpec : ShouldSpec() {
 
+    fun sum(a: Int, b: Int): Int = a + b
+
     init {
-        "Test foo" {
-            should("my first test") {
-                true shouldBe true
+        "Sum properties" {
+            should("keep the associative property") {
+                //TODO test the associative property
+            }
+            should("keep the commutative property") {
+                //TODO test the commutative property
+            }
+            should("have an identity value") {
+                //TODO test the identity property
+            }
+            should("actually sum a couple numbers") {
+                //TODO test the sum
             }
         }
     }
