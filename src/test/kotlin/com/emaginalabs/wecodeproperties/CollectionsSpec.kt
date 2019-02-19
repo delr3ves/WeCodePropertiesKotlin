6865167c9ad22ba358264890a5c75666f8709361
Sergio Arroyo <sergio.arroyo.cuevas@gmail.com>
2019-02-19 22:44:31 +0100

    Define collection properties

---
+++ src/test/kotlin/com/emaginalabs/wecodeproperties/CollectionsSpec.kt
@@ -0,0 +1,28 @@
+package com.emaginalabs.wecodeproperties
+
+import io.kotlintest.specs.ShouldSpec
+
+class CollectionsSpec : ShouldSpec() {
+
+    init {
+        "Collections" {
+            should("contain one more element after insertion") {
+            }
+
+            should("contain the inserted element") {
+            }
+
+            should("have an element less after deletion") {
+            }
+
+            should("not contain the removed element") {
+            }
+
+            should("contain the items of the two collections after a concatenation") {
+            }
+
+            should("no duplicated elements when convert to a set") {
+            }
+        }
+    }
+}
