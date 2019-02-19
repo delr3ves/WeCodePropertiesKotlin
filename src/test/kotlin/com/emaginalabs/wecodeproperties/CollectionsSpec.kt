a474bb85db54dbc8a4be52c98b55f0568e9a29a6
Sergio Arroyo <sergio.arroyo.cuevas@gmail.com>
2019-02-19 23:10:23 +0100

    Solve collection properties

--- src/test/kotlin/com/emaginalabs/wecodeproperties/CollectionsSpec.kt
+++ src/test/kotlin/com/emaginalabs/wecodeproperties/CollectionsSpec.kt
@@ -1,5 +1,6 @@
 package com.emaginalabs.wecodeproperties
 
+import io.kotlintest.properties.forAll
 import io.kotlintest.specs.ShouldSpec
 
 class CollectionsSpec : ShouldSpec() {
@@ -7,21 +8,47 @@ class CollectionsSpec : ShouldSpec() {
     init {
         "Collections" {
             should("contain one more element after insertion") {
+                forAll { list: List<Int>, a: Int ->
+                    (list + a).contains(a)
+                }
             }
 
             should("contain the inserted element") {
+                forAll { list: List<Int>, a: Int ->
+                    (list + a).size == list.size + 1
+                }
             }
 
             should("have an element less after deletion") {
+                forAll { list: List<Int>, a: Int ->
+                    val numberOfAs = list.count { it == a }
+                    val listIncludingA = list + a
+                    val listWithNoAs = listIncludingA.filter { it != a }
+                    listWithNoAs.size == list.size - numberOfAs
+                }
             }
 
             should("not contain the removed element") {
+                forAll { list: List<Int>, a: Int ->
+                    val listIncludingA = list + a
+                    val listWithNoAs = listIncludingA.filter { it != a }
+                    !listWithNoAs.contains(a)
+                }
             }
 
             should("contain the items of the two collections after a concatenation") {
+                forAll { list1: List<String>, list2: List<String> ->
+                    val joinedList = list1 + list2
+                    joinedList.containsAll(list1) && joinedList.containsAll(list2)
+                }
             }
 
             should("no duplicated elements when convert to a set") {
+                forAll { list: List<Int> ->
+                    val asSet = list.toSet()
+                    val distinct = list.distinct()
+                    asSet.containsAll(distinct) && distinct.containsAll(asSet)
+                }
             }
         }
     }
